e8d96d78bbffb0f9bd08227b473413510826835c
miniys <miniys@bk.ru>
2017-11-25 11:28:24 +0700

    Версия 2.0.1.34 - R23.13

--- MeasurePointsExportPultLINQ/MeasurePointsExportPultLINQ/Properties/AssemblyInfo.cs
+++ MeasurePointsExportPultLINQ/MeasurePointsExportPultLINQ/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1.33")]
-[assembly: AssemblyFileVersion("2.0.1.33")]
+[assembly: AssemblyVersion("2.0.1.34")]
+[assembly: AssemblyFileVersion("2.0.1.34")]
