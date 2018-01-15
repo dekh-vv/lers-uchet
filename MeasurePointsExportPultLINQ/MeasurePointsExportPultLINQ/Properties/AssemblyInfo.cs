fa91a6a80533f87af0277bbdd336f03e642cc70e
Dekh-VV <miniys@bk.ru>
2018-01-15 16:27:47 +0700

    Версия 2.0.1.35 - R24.09

--- MeasurePointsExportPultLINQ/MeasurePointsExportPultLINQ/Properties/AssemblyInfo.cs
+++ MeasurePointsExportPultLINQ/MeasurePointsExportPultLINQ/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Экспорт точек учета ЛЭРС Учет в Универсальный пульт")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
